b9a733ed82cb21fa0557be8469fd6ae922c86de9
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-17 18:27:11 -0600

    Creando servidor

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+console.log('me llamo ivan vasquez jeje')
+app.listen(4000,()=>{
+    console.log('Corriendo en el servidor 4000')
+})
