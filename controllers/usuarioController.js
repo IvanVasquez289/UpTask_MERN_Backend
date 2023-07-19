bd4bf3decb7b4b7ba8d5a4ec2c34e3969d0de227
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-18 23:00:09 -0600

    Evitar registros duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,13 @@
 import Usuario from '../models/Usuario.js'
 const registrar = async (req,res) => {
+
+    const {email} = req.body;
+    const existeUsuario = await Usuario.findOne({email:email})
+    if(existeUsuario){
+        const error = new Error('Usuario ya registrado')
+        return res.status(400).json({msj:error.message})
+    }
+    
     try {
         const usuario = new Usuario(req.body)
         const usuarioAlmacenado = await usuario.save()
