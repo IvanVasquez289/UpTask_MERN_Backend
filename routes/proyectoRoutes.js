f20f5315abbda1b6cd54ca6902acd9c125273a81
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-22 22:03:18 -0600

    Creando el routing y metodos de Proyecto

---
+++ routes/proyectoRoutes.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import checkAuth from '../middlewares/checkAuth.js'
+import {
+    obtenerProyectos,
+    nuevoProyecto,
+    obtenerProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas
+} from '../controllers/proyectoController.js'
+
+const router = express.Router()
+
+router.get('/',checkAuth,obtenerProyectos)
+router.post('/',checkAuth,nuevoProyecto)
+
+router
+    .route('/')
+    .get(checkAuth,obtenerProyectos)
+    .post(checkAuth,nuevoProyecto)
+
+router
+    .route('/:id')
+    .get(checkAuth,obtenerProyecto)
+    .put(checkAuth,editarProyecto)
+    .delete(checkAuth,eliminarProyecto)
+
+router.get('/tareas/:id',checkAuth,obtenerTareas)
+
+router.put('/agregar-colaborador/:id',checkAuth,agregarColaborador)
+router.put('/eliminar-colaborador/:id',checkAuth,eliminarColaborador)
+
+export default router
