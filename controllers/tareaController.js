832466f6decac9f08fc3409534e539aae8e4a0da
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-24 22:03:26 -0600

    Editar una tarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -57,7 +57,39 @@ const obtenerTarea = async (req,res) => {
 }
 
 const editarTarea = async (req,res) => {
+    const {id} = req.params
+    const valid = mongoose.Types.ObjectId.isValid(id)
 
+    if(!valid){
+        const error = new Error('ID INVALIDO PARA MONGO')
+        return res.status(404).json({msj: error.message})
+    }
+
+    const tarea = await Tarea.findById(id).populate('proyecto')
+
+    if(!tarea){
+        const error = new Error('LA TAREA NO EXISTE')
+        return res.status(404).json({msj: error.message})
+    }
+
+    if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error('ACCION NO VALIDA')
+        return res.status(403).json({msj: error.message})
+    }
+
+    const propsToUpdate = Object.keys(req.body)
+    try {
+        propsToUpdate.forEach(prop => {
+            if( prop in tarea){
+                tarea[prop] = req.body[prop]
+            }
+        })
+        await tarea.save()
+    } catch (error) {
+        console.log(error)
+    }
+
+    res.json(tarea)
 }
 
 const eliminarTarea = async (req,res) => {
