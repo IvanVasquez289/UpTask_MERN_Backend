3edb75ea2a523943691aa144b12b52a9f714b8b0
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-09-05 23:24:24 -0600

    Agregar el ID de la tarea en Proyecto

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -25,6 +25,9 @@ const nuevaTarea = async (req,res) => {
 
     try {
         const tareaAlmacenada = await Tarea.create(req.body)
+        // Almacenar Id de la tarea en el proyecto
+        existeProyecto.tareas.push(tareaAlmacenada._id)
+        await existeProyecto.save()
         res.json(tareaAlmacenada)
     } catch (error) {
         console.log(error)
